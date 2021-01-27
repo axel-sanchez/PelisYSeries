f7034ebd2b21aefc80982a271847071c31e24521
Axel <axelsan97@hotmail.com>
2021-01-26 23:38:11 -0300

    clean

--- app/src/main/java/com/example/pelisyseries/data/service/ApiService.kt
+++ app/src/main/java/com/example/pelisyseries/data/service/ApiService.kt
@@ -13,45 +13,18 @@ import retrofit2.http.Query
  * @author Axel Sanchez
  */
 interface ApiService {
-    /**
-     * Api que devuelve las peliculas más populares
-     * @param [apiKey] código que nos da la página de la api
-     * @return devuelve un [Result]
-     */
     @GET("movie/popular")
     suspend fun getPopular(@Query("api_key") apiKey: String): Response<Result>
 
-    /**
-     * Api que devuelve las peliculas mejor calificadas
-     * @param [apiKey] código que nos da la página de la api
-     * @return devuelve un [Result]
-     */
     @GET("movie/top_rated")
     suspend fun getTopRated(@Query("api_key") apiKey: String): Response<Result>
 
-    /**
-     * Api que devuelve las peliculas próximas a estrenar
-     * @param [apiKey] código que nos da la página de la api
-     * @return devuelve un [Result]
-     */
     @GET("movie/upcoming")
     suspend fun getUpcoming(@Query("api_key") apiKey: String): Response<Result>
 
-    /**
-     * Api que devuelve las peliculas que coinciden con la búsqueda
-     * @param [apiKey] código que nos da la página de la api
-     * @param [query] es el nombre de la pelicula
-     * @return devuelve un [Result]
-     */
     @GET("search/movie")
     suspend fun search(@Query("api_key") apiKey: String, @Query("query") query: String): Response<Result>
 
-    /**
-     * Api que devuelve el video de una pelicula
-     * @param [apiKey] código que nos da la página de la api
-     * @param [id] es el id de la pelicula
-     * @return devuelve un [VideoResult]
-     */
     @GET("movie/{movie_id}/videos")
     suspend fun getVideo(@Path("movie_id") id: String, @Query("api_key") apiKey: String): Response<VideoResult>
 
@@ -59,45 +32,18 @@ interface ApiService {
     //TODO: FUNCIONES CREADAS PARA HACER TEST
 
 
-    /**
-     * Api de test que devuelve las peliculas más populares
-     * @param [apiKey] código que nos da la página de la api
-     * @return devuelve un [Call] de [Result]
-     */
     @GET("movie/popular")
     fun getPopularTest(@Query("api_key") apiKey: String): Call<Result>
 
-    /**
-     * Api de  test que devuelve las peliculas mejor calificadas
-     * @param [apiKey] código que nos da la página de la api
-     * @return devuelve un [Call] de [Result]
-     */
     @GET("movie/top_rated")
     fun getTopRatedTest(@Query("api_key") apiKey: String): Call<Result>
 
-    /**
-     * Api de test que devuelve las peliculas próximas a estrenar
-     * @param [apiKey] código que nos da la página de la api
-     * @return devuelve un [Call] de [Result]
-     */
     @GET("movie/upcoming")
     fun getUpcomingTest(@Query("api_key") apiKey: String): Call<Result>
 
-    /**
-     * Api de test que devuelve las peliculas que coinciden con la búsqueda
-     * @param [apiKey] código que nos da la página de la api
-     * @param [query] es el nombre de la pelicula
-     * @return devuelve un [Call] de [Result]
-     */
     @GET("search/movie")
     fun searchTest(@Query("api_key") apiKey: String, @Query("query") query: String): Call<Result>
 
-    /**
-     * Api de test que devuelve el video de una pelicula
-     * @param [apiKey] código que nos da la página de la api
-     * @param [id] es el id de la pelicula
-     * @return devuelve un [Call] de [VideoResult]
-     */
     @GET("movie/{movie_id}/videos")
     fun getVideoTest(@Path("movie_id") id: String, @Query("api_key") apiKey: String): Call<VideoResult>
 
--- app/src/main/java/com/example/pelisyseries/ui/MainActivity.kt
+++ app/src/main/java/com/example/pelisyseries/ui/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.pelisyseries.ui
 
-import android.os.Build
 import android.os.Bundle
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pelisyseries.R
 
